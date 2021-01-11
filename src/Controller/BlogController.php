a0cbc1e5aef58421ca20fe0eb9e5e8cdd60f5dd1
bilal07 <mouadine.bilal@gmail.com>
2021-01-11 21:18:47 +0100

    manipulate Article Entity

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,19 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
+
     /**
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function index(ArticleRepository $repo): Response
     {
+        $articles = $repo->findAll();
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'articles' => $articles,
         ]);
     }
 
@@ -25,5 +29,16 @@ class BlogController extends AbstractController
      */
     public function home() {
         return $this->render('blog/home.html.twig');
+    }
+
+    /**
+     * Undocumented function
+     * @Route("/blog/{id}", name="blog_show")
+     * @return void
+     */
+    public function show(Article $article) {
+        return $this->render('blog/show.html.twig',[
+            'article' => $article,
+        ]);
     }
 }
